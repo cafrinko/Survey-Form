86e6e1695ad4ec2e93b2402213665848c6fc225b
Catherine Karra <catherine.karra@gmail.com>
2016-03-23 22:13:57 -0700

    add color and date picker, select boxes, and a bit of css styles

--- js/surveyscripts.js
+++ js/surveyscripts.js
@@ -2,11 +2,18 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var name1Input = $("input#name1").val();
     var name2Input = $("input#name2").val();
-    var vegetablesInput = $("input:radio[name=vegetables]:checked").val();
+    var bornInput = $("input#born").val();
+    var colorInput = parseInt($("input#color").val());
+    var vegetableInput = $("input:radio[name=vegetable]:checked").val();
+    var genre = $("#genre").val();
+    $("form").remove();
 
     $(".name1").text(name1Input);
     $(".name2").text(name2Input);
-    $(".vegetables").text(vegetablesInput);
+    $(".born").text(bornInput);
+    $(".color").text(colorInput);
+    $(".vegetable").text(vegetableInput);
+    $(".genre").text(genre);
 
     $("#results").show();
     event.preventDefault();
